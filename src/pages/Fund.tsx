8f7133780fb03f864a838688a95d8a99d66522c6
semgoSE <semgoSE@gmail.com>
2023-03-26 13:29:18 +0300

    finish fund

--- src/pages/Fund.tsx
+++ src/pages/Fund.tsx
@@ -8,16 +8,36 @@ import {
     CardContent,
     CardHeader,
     Container,
-    Grid,
-    Stack,
+    Grid, Paper,
+    Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow,
     Typography
 } from '@mui/material';
 // components
 import {useNavigate} from "react-router-dom";
 import ChartLine from "../sections/fund/ChartLine";
 import Markdown from "../components/markdown";
+import Scrollbar from "../components/scrollbar";
 
-// ----------------------------------------------------------------------
+const rowsAssets = [
+    {
+        asset: "BTC",
+        amount: "50,012",
+        value: "$300,012",
+        state: "Staked on Acala",
+    },
+    {
+        asset: "BTC",
+        amount: "50,012",
+        value: "$300,012",
+        state: "Staked on Acala",
+    },
+    {
+        asset: "BTC",
+        amount: "50,012",
+        value: "$300,012",
+        state: "Staked on Acala",
+    },
+]
 
 export default function Fund() {
     const nav = useNavigate();
@@ -77,7 +97,8 @@ export default function Fund() {
                         <Card>
                             <CardHeader title="Description" />
                             <CardContent>
-                                <Markdown children="
+                             <Scrollbar sx={{ height: 335 }}>
+                                 <Markdown children="
                                     <h3>General</h3>
                                      <span>
                                       Our crypto fund is designed to heavily invest in staking various assets on the OkChain. Staking is a process where investors hold their assets in a particular cryptocurrency wallet, and the assets are used to validate transactions on the blockchain network. In return, the investors receive rewards in the form of interest or tokens. The OkChain is a fast, reliable, and secure blockchain network that allows investors to stake their assets and earn rewards.
@@ -86,13 +107,59 @@ export default function Fund() {
                                     Investing in a crypto fund that heavily invests in staking various assets on the OkChain carries some risks. The value of cryptocurrencies can be volatile and subject to market fluctuations, which can cause significant losses to investors. Staking can also be risky, as the validator nodes may face technical issues, network failures, or security threats that can impact the rewards earned by investors. Moreover, regulatory changes or legal issues may impact the usability and adoption of cryptocurrencies.
 
                                 " />
+                             </Scrollbar>
                             </CardContent>
                         </Card>
                     </Grid>
 
-                    <Grid xs={12} md={6} item>
+                    <Grid xs={12} md={7} item>
+                        <Card>
+                            <CardHeader title="Fund's Portfolio" />
+                            <CardContent>
+                                <TableContainer component={Paper}>
+                                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                                        <TableHead>
+                                            <TableRow>
+                                                <TableCell align="right">Asset</TableCell>
+                                                <TableCell align="right">Amount</TableCell>
+                                                <TableCell align="right">Value</TableCell>
+                                                <TableCell align="right">State</TableCell>
+                                            </TableRow>
+                                        </TableHead>
+                                        <TableBody>
+                                            {rowsAssets.map((row, i) => (
+                                                <TableRow
+                                                    key={i}
+                                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                                >
+                                                    <TableCell align="right">{row.asset}</TableCell>
+                                                    <TableCell component="th" scope="row">
+                                                        {row.amount}
+                                                    </TableCell>
+                                                    <TableCell align="right">{row.value}</TableCell>
+                                                    <TableCell align="right">{row.state}</TableCell>
+                                                </TableRow>
+                                            ))}
+                                        </TableBody>
+                                    </Table>
+                                </TableContainer>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+
+                    <Grid xs={12} md={5} item>
                         <Card>
                             <CardHeader title="My share" />
+                            <CardContent>
+                                <Stack direction="row" justifyContent="space-between" mt={1}>
+                                    <Typography>Your share: 150 APF</Typography>
+                                    <Typography>24H change: Up 3%</Typography>
+                                </Stack>
+                                <Stack direction="row" justifyContent="space-between" mt={1}>
+                                    <Typography>Value: $3,131</Typography>
+                                    <Typography>All time: Up 11%</Typography>
+                                </Stack>
+                            </CardContent>
                         </Card>
                     </Grid>
                 </Grid>
